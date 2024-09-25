b7356ec28e3e79606b89c97bba7173333bee6f6e
Cameron Ramsell <11411400+cameron-ramsell@users.noreply.github.com>
2024-09-25 07:58:13 +0100

    Add nvim config

---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "rust_analyzer" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.rust_analyzer.setup({})
+
+            vim.keymap.set("n", "<leader><leader>", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
+        end
+    }
+}
